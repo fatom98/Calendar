37b350d8ce315b4ce844cf945cf00ea80b06fe2a
Fatih Akbulut <akbulut571@gmail.com>
2020-09-01 13:40:41 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,136 @@
+from tkinter import *
+from tkinter.ttk import Treeview
+from tkinter.messagebox import *
+from datetime import datetime
+import webbrowser, json
+
+class GUI(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.initUI()
+
+
+    def initUI(self):
+        self.pack(expand = True, fill = BOTH)
+
+        frame1 = Frame(self)
+        frame1.pack(fill = X)
+
+        frame2 = Frame(self)
+        frame2.pack(fill = X, pady = 20)
+
+        frame3 = Frame(self)
+        frame3.pack(fill = X, pady = 30)
+
+        Label(frame1, text = "Ders Takvimi", font = "ComicSans 16", bg = "#3b5998", fg = "white").pack(fill = X)
+
+        tree = Treeview(frame2)
+        tree['show'] = "headings"
+        tree["columns"] = ("week", "one" , "two", "three", "four", "five")
+
+        treeWidth = 134
+
+        tree.column("week", width = treeWidth, anchor = W)
+        tree.column("one", width = treeWidth, anchor = W)
+        tree.column("two", width = treeWidth, anchor = W)
+        tree.column("three", width = treeWidth, anchor = W)
+        tree.column("four", width = treeWidth, anchor = W)
+        tree.column("five", width = treeWidth, anchor = W)
+
+        tree.heading("week", text = "")
+        tree.heading("one", text = "1.Ders")
+        tree.heading("two", text = "2.Ders")
+        tree.heading("three", text = "3.Ders")
+        tree.heading("four", text = "4.Ders")
+        tree.heading("five", text = "5.Ders")
+
+        tree.pack()
+
+        tree.insert("", "end", text = "", values = ("Pazartesi", "Türkçe", "Matematik", "İngilizce", "Türkçe", "İngilizce"))
+        tree.insert("", "end", text = "", values = ("Salı", "Türkçe", "Müzik", "Beden Eğitimi", "Türkçe", "İngilizce"))
+        tree.insert("", "end", text = "", values = ("Çarşamba", "Türkçe", "İngilizce", "Türkçe", "Kuran", "Matematik"))
+        tree.insert("", "end", text = "", values = ("Perşembe", "Türkçe", "Görsel Sanatlar", "İngilizce", "Matematik", "Matematik"))
+        tree.insert("", "end", text = "", values = ("Cuma", "Hayat Bilgisi", "Kuran", "İngilizce", "Matematik", "Hayat Bilgisi"))
+
+        lecture = Button(frame3, text = "Derse Gir", command = self.which, height = 3, width = 15)
+        lecture.pack()
+
+    def which(self):
+        day = datetime.now().strftime("%A")
+        hour = datetime.now().strftime("%H")
+        minute = datetime.now().strftime("%M")
+
+        with open("docs/calendar.json", "r") as file:
+            calendar = json.load(file)
+
+        if day not in calendar:
+            showerror("Hata","Bugün ders yok")
+
+        elif int(hour) < 13 or int(hour) > 17:
+            showerror("Hata", "Ders saati değil")
+
+        else:
+            ders = self.number(int(hour), int(minute))
+
+            if ders is -1:
+                showerror("Hata", "Ders saati değil")
+
+            else:
+                url = calendar.get(day).get(str(ders))
+                webbrowser.register('chrome', None, webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+                webbrowser.get('chrome').open(url)
+
+    def number(self, hour: int, minute: int) -> int:
+
+        if hour is 13:
+            return 1
+
+        elif hour is 14:
+            if 0 <= minute < 30:
+                return 1
+
+            elif minute >= 30:
+                return 2
+
+
+        elif hour is 15:
+
+            if 0 <= minute < 10:
+                return 2
+
+            elif 10 <= minute < 50:
+                return 3
+
+            else:
+                return 4
+
+        elif hour is 16:
+
+            if 0 <= minute < 30:
+                return 4
+
+            elif 30 <= minute:
+                return 5
+
+
+        else:
+            if 0 <= minute < 10:
+                return 5
+
+            else:
+                return -1
+
+
+
+
+if __name__ == '__main__':
+    root = Tk()
+    app = GUI(root)
+    root.title("Ders Takvimi")
+    screenWidth = root.winfo_screenwidth()
+    screenHeight = root.winfo_screenheight()
+    width = 804
+    height = 600
+    root.geometry(f"{width}x{height}+{ int(screenWidth/2 - width/2) }+{ int(screenHeight/2 - height/2) - 30 }")
+    root.iconphoto(True, PhotoImage(file = "images/icon.png"))
+    root.mainloop()
