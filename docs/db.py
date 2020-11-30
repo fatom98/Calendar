688d11a01c3836a594f43ce11a81e3a3111397e1
Fatih Akbulut <akbulut571@gmail.com>
2020-11-30 09:43:52 +0300

    schedue change

--- docs/db.py
+++ docs/db.py
@@ -10,7 +10,7 @@ eng = ("https://us04web.zoom.us/j/9018960986?pwd=Qzk1Sm9kMmxId0NyNnVlR0hSbnRJdz0
 pe = ("https://us04web.zoom.us/j/3452723043?pwd=bE03cktJOHZ2Lzg2Tmt0amREVGpEdz09", "Beden Eğitimi")
 life = ("https://us04web.zoom.us/j/8391860248?pwd=UFQ3bmtodVZmMm1NN2wxOFcreWpwZz09", "Hayat Bilgisi")
 robot = ("https://us04web.zoom.us/j/5609504756?pwd=MVMzTU8xaTlBV1U3Yjd1RzRVVHRPdz09", "Robotik Kodlama")
-skills = ("https://us04web.zoom.us/j/7918989451?pwd=VUNEdkE0R0VxQisyVU9TZitQSU1QQT09", "Skills")
+skills = ("https://us04web.zoom.us/j/2937880883?pwd=MTVLTTdESTUvbzNIUHdmUVNDT3N3QT09", "Skills")
 kuran = ("https://us04web.zoom.us/j/4103999829", "Kuran")
 gor = ("https://us04web.zoom.us/j/3314303377", "Görsel Sanatlar")
 mus = ("https://us04web.zoom.us/j/4975962849", "Müzik")
@@ -18,7 +18,7 @@ value = ("https://us04web.zoom.us/j/6868293773", "Değerler Eğitimi")
 
 programs = [
     [mat, fen, pe, eng, tur, kuran, tur],
-    [tur, eng, mat, skills, life, tur, kuran],
+    [tur, eng, skills, mat, life, tur, kuran],
     [mat, fen, life, mus, eng, tur, eng],
     [tur, robot, tur, eng, value, gor, mat],
     [fen, mat, tur, life, tur, eng, eng]
